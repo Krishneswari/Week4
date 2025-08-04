3301550097c53166a11c1133bb7d85eafa37208e
Krishneswari <krishnaseelam6@gmail.com>
2025-08-04 15:03:51 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello Welcome to DevOps Lab");
 		System.out.println("Krishneswari Seelam");
+		System.out.println("Srujanaa");
 	}
 
 }
+
